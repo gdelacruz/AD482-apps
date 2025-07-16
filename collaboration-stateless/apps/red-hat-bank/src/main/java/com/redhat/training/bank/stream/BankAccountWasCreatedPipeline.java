e53ce139608ce24988a92b2d500bd90f888aea21
German de la Cruz <ger@Padme.local>
2025-07-16 12:49:56 -0300

    v0

--- collaboration-stateless/apps/red-hat-bank/src/main/java/com/redhat/training/bank/stream/BankAccountWasCreatedPipeline.java
+++ collaboration-stateless/apps/red-hat-bank/src/main/java/com/redhat/training/bank/stream/BankAccountWasCreatedPipeline.java
@@ -16,7 +16,7 @@ import javax.enterprise.event.Observes;
 import javax.transaction.Transactional;
 
 @ApplicationScoped
-public class BankAccountWasCreatedPipeline extends  StreamProcessor {
+public class BankAccountWasCreatedPipeline extends StreamProcessor {
     private static final Logger LOGGER = Logger.getLogger(BankAccountWasCreatedPipeline.class);
 
     // Reading topic
@@ -27,16 +27,25 @@ public class BankAccountWasCreatedPipeline extends  StreamProcessor {
     void onStart(@Observes StartupEvent startupEvent) {
         StreamsBuilder builder = new StreamsBuilder();
 
-        ObjectMapperSerde<BankAccountWasCreated> eventSerde
-                = new ObjectMapperSerde<>(BankAccountWasCreated.class);
+        ObjectMapperSerde<BankAccountWasCreated> eventSerde = new ObjectMapperSerde<>(BankAccountWasCreated.class);
 
         // TODO: Update the account type on each event
+        builder.stream(
+                BANK_ACCOUNT_WAS_CREATED_TOPIC,
+                Consumed.with(Serdes.Long(), eventSerde)).foreach((key, creation) -> {
+                    updateAccountTypeFromEvent(creation);
+                });
 
         // TODO: Create a Kafka streams and start it
+        streams = new KafkaStreams(
+                builder.build(),
+                generateStreamConfig());
+        streams.start();
     }
 
     void onStop(@Observes ShutdownEvent shutdownEvent) {
         // TODO: Close the stream on shutdown
+        streams.close();
     }
 
     @Transactional
@@ -49,8 +58,7 @@ public class BankAccountWasCreatedPipeline extends  StreamProcessor {
             LOGGER.infov(
                     "Updated Bank Account - ID: {0} - Type: {1}",
                     event.id,
-                    entity.profile
-            );
+                    entity.profile);
         } else {
             LOGGER.infov("Bank Account with id {0} not found!", event.id);
         }
